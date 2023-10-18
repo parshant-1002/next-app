bbca222f57bd976299e123eabefc63165eeb81d6
parshant-1002 <parshant.sharama@chicmic.co.in>
2023-10-18 18:30:35 +0530

    starting crud app

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,11 @@
 "use client";
-import Counter from "./child/Counter";
-import { useState } from "react";
 
 export default function Home() {
-  const [num, setNum] = useState(0);
-  const handleDec = () => {
-    if(num>0){
-      setNum((prevNum) => prevNum - 1);
-    }
-  };
-  const handleInc = () => {
-    setNum((prevNum) => prevNum + 1);
-  };
-  const handleReset = () => {
-    setNum(0);
-  };
+  
   return (
     <div>
-      <div className="w-100 text-center text-4xl">Hello I am counter</div>
-      <Counter
-        num={num}
-        handleDec={handleDec}
-        handleInc={handleInc}
-        handleReset={handleReset}
-      />
+      <div className="w-100 text-center text-4xl">Hello I am crud-app</div>
+      
     </div>
   );
 }
